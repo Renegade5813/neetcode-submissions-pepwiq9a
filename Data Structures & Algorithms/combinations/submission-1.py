27bf9dbea8b336d80a4134aa0268ee926b92e526
Aditya <111658716+Renegade5813@users.noreply.github.com>
2026-04-14 12:41:30 -0400

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+
+        def backtrack(start, path):
+            if len(path) == k:
+                res.append(path[:])
+                return 
+            for num in range(start, n + 1):
+                if num not in path:
+                    path.append(num)
+                    backtrack(num + 1, path)
+                    path.pop()
+            
+        backtrack(1, [])
+        return res
